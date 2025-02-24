02c0c193c7c5e0bea0f0edd7cdd3d813ebbbb405
XelaRonnoc <100101145+XelaRonnoc@users.noreply.github.com>
2025-02-24 19:06:31 +1100

    playing with shapes

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,12 +7,18 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+//   float d = distance(p, vec2(0.3, 0.5));     // calculate distance to midpoint
+//  this and line below draw square
+	vec2 v = abs(p - vec2(0.5, 0.5)); // this gets all the xy distances from point to (0.5,0.5) screen center
+	float d = max(v.x, v.y); // this then gets the maximum value out of x or y from v above
+	// if you change to min you get a cross as it will colour anything where at least one of points is less than the if condition below   
+	float diamond = v.x + v.y; // this means that the vertex can be further away in one dimension from the center than the side can as the vertex is only summed with 0
+	
   
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
+   if (diamond < 0.28) {
+      o_colour = vec4(0,1,0, 0);
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(0,0,1,1); // BLACK
    }
 }
